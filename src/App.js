4dc62f4cfca5e04056e1ce528ce984a0f6cc3f6d
Alex <bache.alex@gmail.com>
2022-02-28 12:55:57 +0000

    removed unneccesary bits
    git commit -m

--- src/App.js
+++ src/App.js
@@ -3,10 +3,6 @@ import "./App.css";
 import { lyrics } from "./lyrics";
 
 class Lyric extends React.Component {
-  constructor(props) {
-    super(props);
-  }
-
   render() {
     return (
       <div className="lyricbox">
